e6bf71ac354cc93e65ef0f5bec24560dde5386c8
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-06-30 06:36:53 +0200

    Added a template literal to show the card details

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 const gallery = document.getElementById('gallery');
 
-/* Reusable function that takes an element and a className as parameters. Just to make it easier to read*/
-
-const createElement = (element, className) => {
-  const el = document.createElement(element);
-  const result = el.classList.add(className);
-  return result;
-};
-
 /* Reusable function that fetches the URL that is passed as a parameter and converts it to json format*/
 
 const fetchData = (url) => fetch(url).then((res) => res.json());
@@ -15,15 +7,23 @@ const fetchData = (url) => fetch(url).then((res) => res.json());
 fetchData('https://randomuser.me/api/?format=SQL&results=12').then((data) => {
   const users = data.results.map((user) => {
     const { first, last } = user.name;
-    const city = user.location.city;
+    const { city, state } = user.location;
     const email = user.email;
     const thumbnail = user.picture.thumbnail;
 
-    const card = createElement('div', 'card');
-    const cardImgContainer = createElement('div', 'card-img-container');
-    const img = createElement('img', 'card-img');
-
-    gallery.appendChild(card);
+    gallery.innerHTML = `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${thumbnail}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${first} ${last}</h3>
+                <p class="card-text">${email}</p>
+                <p class="card-text cap">${city}, ${state}</p>
+            </div>
+        </div>
+    `;
+    console.log(gallery);
   });
 
   return users;
