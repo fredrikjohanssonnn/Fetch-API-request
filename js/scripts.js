d673ad2c1cea6eb670367f4c8b5479154f6e497d
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-07-01 22:51:42 +0200

    Updated the fetch to return the data in order to be able to use it in other functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,50 @@
 const gallery = document.getElementById('gallery');
-
+const card = document.querySelectorAll('.card');
+let users = null;
 /* Reusable function that fetches the URL that is passed as a parameter and converts it to json format*/
 
-const fetchData = (url) => fetch(url).then((res) => res.json());
+const fetchData = async (url) => await fetch(url).then((res) => res.json());
 
 fetchData('https://randomuser.me/api/?format=SQL&results=12')
   .then((data) => {
-    const users = data.results
-      .map((user) => {
-        /* Destructuring objects for readability */
-
-        const { first: firstname, last: lastname } = user.name;
-        const { city, state } = user.location;
-        const email = user.email;
-        const thumbnail = user.picture.thumbnail;
-
-        return `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="${thumbnail}" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${firstname} ${lastname}</h3>
-                <p class="card-text">${email}</p>
-                <p class="card-text cap">${city}, ${state}</p>
-            </div>
-        </div>
-    `;
-      })
-      .join('');
-
-    gallery.innerHTML = users;
+    users = data.results;
+    displayUsers();
+    return users;
   })
   .catch((err) => console.error(err));
 
+const displayUsers = () => {
+  const userIndex = users
+    .map((user) => {
+      const { first: firstname, last: lastname } = user.name;
+      const { city, state } = user.location;
+      const email = user.email;
+      const thumbnail = user.picture.thumbnail;
+
+      return `
+      <div class="card">
+          <div class="card-img-container">
+              <img class="card-img" src="${thumbnail}" alt="profile picture">
+          </div>
+          <div class="card-info-container">
+              <h3 id="name" class="card-name cap">${firstname} ${lastname}</h3>
+              <p class="card-text">${email}</p>
+              <p class="card-text cap">${city}, ${state}</p>
+          </div>
+      </div>
+  `;
+    })
+    .join('');
+
+  gallery.innerHTML = userIndex;
+};
+
+const showModal = () => {
+  console.log(card);
+};
+
 gallery.addEventListener('click', (e) => {
   if (e.target !== gallery) {
+    showModal();
   }
 });
